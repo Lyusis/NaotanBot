e9caae6f1395d30d82802c31126a184f7cb6b7cd
Lyusis <wuliaodetuanzi@gmail.com>
2021-06-22 20:48:15 +0900

    fix: network config

--- src/monitor/fetcher/fetcher.go
+++ src/monitor/fetcher/fetcher.go
@@ -1,17 +1,24 @@
 package fetcher
 
 import (
+	"config"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"monitor/logger"
 	"net/http"
+	"time"
 )
 
 func GetFetcher(url string) ([]byte, error) {
 
-	response, responseError := http.Get(url)
+	client := http.Client{
+		Timeout: time.Duration(config.Wait/5) * time.Second,
+	}
+
+	response, responseError := client.Get(url)
 	if responseError != nil {
+		logger.Error("未响应请求或响应失败, 将在下次轮询中再次请求", false, responseError)
 		return nil, responseError
 	}
 
